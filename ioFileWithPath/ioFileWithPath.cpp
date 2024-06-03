15d9521b24eec28bdaf2a7da42ab192c2b934d34
M S I <M S I@MSI>
2024-06-03 10:15:10 +0700

    unlimited loop untuk menulis

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -12,6 +12,13 @@ int main() {
 
 	ofstream outfile;
 	outfile.open(NamaFile + ".txt", ios::out);
-}
 
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+	while (true) {
+		cout << "-";
+		getline(cin, baris);
+		if (baris == "q") break;
+		outfile << baris << endl;
+	}
 	
