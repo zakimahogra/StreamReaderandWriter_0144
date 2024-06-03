68e43858a38f52379dec787af8cf01a9e6b21f18
M S I <M S I@MSI>
2024-06-03 10:07:40 +0700

    melakukan perilangan setiap kata dan menampilkan

--- ioFile/ioFile.cpp
+++ ioFile/ioFile.cpp
@@ -21,4 +21,15 @@ int main() {
 	ifstream infile;
 	infile.open("contohfile.txt");
 
-	
+	cout << endl << ">= Membuka dan membaca file" << endl;
+	if (infile.is_open())
+	{
+		while (getline(infile, baris))
+		{
+			cout << baris << '\n';
+		}
+		infile.close();
+	}
+	else cout << "Unable to open file";
+	return 0;
+}
