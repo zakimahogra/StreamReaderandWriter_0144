4372f9618f25daab27516d3cc5418cf15bdc5823
M S I <M S I@MSI>
2024-06-03 10:25:42 +0700

    pesan array integer 3 elemen

---
+++ ExceptionStandart/ExceptionStandart.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <exception>
+#include <array>
+using namespace std;
+
+int main()
+{
+	cout << "Awal Program" << endl;
+	try {
+		array<int, 3> data = { 1,2,3 };
+
+	}
+}
--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -26,6 +26,17 @@ int main() {
 	ifstream infile;
 	infile.open(NamaFile + ".txt", ios::in);
 
-	
+	cout << endl << ">= Membuka dan membaca file" << endl;
+	if (infile.is_open())
+	{
+		while (getline(infile, baris))
+		{
+			cout << baris << '\n';
+		}
+		infile.close();
+	}
+	else cout << "Unable to open file";
+	return 0;
+}
 
 
